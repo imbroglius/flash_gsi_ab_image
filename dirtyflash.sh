5bbfaac1316f9b4bbfaacdec2f627ea6f7cab11c
imbroglius <163344413+imbroglius@users.noreply.github.com>
2024-07-07 22:47:56 +0200

    Create dirtyflash.sh

---
+++ dirtyflash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo
+echo "--------------------------------------"
+echo "          Dirty Flash Script          "
+echo "                  by                  "
+echo "              Imbroglius              "
+echo "--------------------------------------"
+echo
+
+set -e
+read -p "Please connect your phone in Fastboot-Mode via USB and press any key"
+fastboot reboot fastboot
+fastboot flash systemm
+fastboot reboot recovery
+
+echo "Your phone will now start in recovery. Please reboot it to system."
